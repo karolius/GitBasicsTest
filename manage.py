113a70010ee83583231e52b6ac7453e0025c998e
karolius <karolius127@gmail.com>
2016-10-26 14:28:59 +0200

    Update manage.py with a note

--- manage.py
+++ manage.py
@@ -19,7 +19,3 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    execute_from_command_line(sys.argv)
-
-    # git te
-    # tessstttt
